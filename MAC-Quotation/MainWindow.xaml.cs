04ee7900b3fe1c8a9998f0f898c9917a678fe8cd
Goldenboy99-XAU <110536745+Goldenboy99-XAU@users.noreply.github.com>
2023-04-11 11:47:23 +0200

    Add project files.

---
+++ MAC-Quotation/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MAC_Quotation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Start a timer that will navigate to the dashboard window after 6 seconds
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(6);
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                var dashboardWindow = new Dashboard();
+                dashboardWindow.Show();
+                Close();
+            };
+            timer.Start();
+        }
+    }
+}
